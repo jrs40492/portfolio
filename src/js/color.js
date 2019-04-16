c4ae28170a5a9c139af6096362be5f6b873d6d1c
Jacob Swanson <jrs40492@gmail.com>
2019-04-15 21:23:43 -0400

    Updates to color background

--- src/js/color.js
+++ src/js/color.js
@@ -1,33 +1,15 @@
 const section = document.getElementById('introduction');
-const pageHeight = window.innerHeight / 255;
-const pageWidth = window.innerWidth / 255;
-
-const inverseColor = value => {
-  return 255 - value;
-};
 
 section.addEventListener('mousemove', e => {
   const x = e.clientX;
   const y = e.clientY;
 
-  const red = x / pageWidth;
-  const green = y / pageHeight;
+  const pageHeight = window.innerHeight / 255;
+  const pageWidth = window.innerWidth / 255;
+
+  const red = Math.round(x / pageWidth);
+  const green = Math.round(y / pageHeight);
   const blue = (red + green) / 2;
 
-  section.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-
-  const background = (red * 299 + green * 587 + blue * 114) / 1000;
-  const color = background >= 128 ? 'black' : 'white';
-
-  section.style.color = color;
-
-  const buttons = section.querySelectorAll('.button');
-  buttons.forEach(button => {
-    button.style.border = `1px solid ${color}`;
-  });
-
-  const links = section.querySelectorAll('a');
-  links.forEach(link => {
-    link.style.color = color;
-  });
+  section.style.backgroundImage = `linear-gradient(to bottom right, rgba(${red}, 0, 0, .6), rgba(0, ${green}, 0, .6), rgba(0, 0, ${blue}, .6))`;
 });
