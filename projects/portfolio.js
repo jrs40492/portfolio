8569b2d6801c5f34ca720dcc543d565a666e31f0
Jacob Swanson <jrs40492@gmail.com>
2023-03-18 19:20:06 -0400

    Fix projects

--- projects/portfolio.js
+++ projects/portfolio.js
@@ -1,20 +1,13 @@
 module.exports = {
   id: "portfolio",
   title: "Portfolio",
-  description: "This website was built to showcase my talents and ability to learn new languages and skills. This was the first time I had used Pug, Grunt, Firebase, and PM2.",
+  description:
+    "This website was built to showcase my talents and ability to learn new languages and skills. This was the first time I had used Pug, Grunt, and Firebase.",
   link: "https://jacobrswanson.com",
   source: "https://github.com/jrs40492/Portfolio",
   start_year: 2018,
   end_year: null,
   image: "portfolio",
   resume: true,
-  tools: [
-    "Pug",
-    "Grunt",
-    "Gitlab CI/CD",
-    "Docker",
-    "Nginx",
-    "Node.js",
-    "SASS"
-  ]
-}
+  tools: ["Pug", "Grunt", "Gitlab CI/CD", "Docker", "Nginx", "Node.js", "SASS"],
+};
--- projects/yori_wine.js
+++ projects/yori_wine.js
@@ -1,12 +1,11 @@
 module.exports = {
   id: "yori-wine",
   title: "Yori Wine Cellars",
-  description: "Assisted the owner with the set up and layout of the website for his wine business in North East, PA. Feel free to take a look and maybe order some delicious wine!",
-  link: "https://childrenofmaryhomeschoolers.org/",
+  description:
+    "Assisted the owner with the set up and layout of the website for his wine business in North East, PA. Feel free to take a look and maybe order some delicious wine!",
+  link: "https://www.yoriwinecellars.com/",
   start_year: 2019,
   end_year: null,
   image: "wine",
-  tools: [
-    "Squarespace"
-  ]
-}
+  tools: ["Squarespace"],
+};
