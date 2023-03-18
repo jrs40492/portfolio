91c578cd6dffe1ea63ed8697a3cfd5acc7a41f6f
Jacob Swanson <jrs40492@gmail.com>
2023-03-18 17:29:23 -0400

    Fix network again

---
+++ work/cover_my_meds.js
@@ -0,0 +1,11 @@
+module.exports = {
+  position: "Software Developer",
+  company: "CoverMyMeds",
+  start_year: 2019,
+  end_year: "Current",
+  tools: ["Ruby on Rails", "Python", "Elixir", "Webpack"],
+  duties: [
+    "Developed code that scraped information using regular expressions.",
+    "Inserted scraped data into backend database to populate website.",
+  ],
+};
